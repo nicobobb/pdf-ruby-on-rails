0c018dfb442ed1f0fc93b0604246f3be46215cdd
nicobobb <nicobobb@gmail.com>
2016-12-04 03:43:22 -0300

    Proyecto general

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Student.create(name:'Juan', last_name: "Alvarez")
+Student.create(name:'Pedro', last_name: "Gonzales")
+Student.create(name:'Lautaro', last_name: "Smith")
+Student.create(name:'Marta', last_name: "De felice")
+Student.create(name:'Amanda', last_name: "Scheine")
